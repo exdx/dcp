042f74a8ed9ec3ea1554b05e61cad9558ddb2d47
Denton24646 <Denton24646@gmail.com>
2022-07-11 11:25:31 -0400

    *: add tokio runtime; downgrade clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use clap::{App, Arg};
 use std::error::Error;
+use docker_api::{Docker};
 
-type DCPResult<T> = Result<T, Box<dyn Error>>;
+pub type DCPResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
     // List of images
-    images: Vec<String>,
+    image: String,
     // Where the download files should be saved on the filesystem. Default "."
     download_path: String,
     // Where the content (files) are in the container filesystem. Default "/"
@@ -21,51 +22,62 @@ pub fn get_args() -> DCPResult<Config> {
         .author("exdx")
         .about("docker cp made easy")
         .arg(
-            Arg::new("images")
-                .name("IMAGES")
+            Arg::with_name("image")
+                .value_name("IMAGE")
                 .help("Container images to extract content from")
                 .required(true)
-                .multiple_values(true)
         )
         .arg(
-            Arg::new("download_path")
-                .name("DOWNLOAD_PATH")
+            Arg::with_name("download_path")
+                .value_name("DOWNLOAD_PATH")
                 .help("Where the contents of the image should be saved on the filesystem")
                 .default_value(".")
-                .short('d')
+                .short("d")
                 .long("download_path")
         )
         .arg(
-            Arg::new("content_path")
-                .name("CONTENT_PATH")
+            Arg::with_name("content_path")
+                .value_name("CONTENT_PATH")
                 .help("Where in the container filesystem the content to extract is")
-                .short('p')
+                .short("p")
                 .default_value("/")
                 .long("content_path")
         )
         .arg(
-            Arg::new("write_to_stdout")
-                .name("WRITE_TO_STDOUT")
+            Arg::with_name("write_to_stdout")
+                .value_name("WRITE_TO_STDOUT")
                 .help("Whether to write to stdout instead of the filesystem")
                 .takes_value(false)
-                .short('w')
+                .short("w")
                 .long("write_to_stdout")
         ).get_matches();
 
-    let images = matches.values_of_lossy("images").unwrap();
+    let image = matches.value_of("image");
     let download_path = matches.value_of("download_path").unwrap().to_string();
     let content_path = matches.value_of("content_path").unwrap().to_string();
     let write_to_stdout = matches.is_present("write_to_stdout");
 
+    let mut img = String::new();
+    match image {
+        Some(mut i) => {
+            img = i.to_string()
+        }
+        None => (),
+    }
+
     Ok(Config {
-        images,
+        image: img,
         download_path,
         content_path,
         write_to_stdout,
     })
 }
 
-pub fn run(config: Config) -> DCPResult<()> {
-    println!("{:?}", config);
+pub async fn run(config: Config) -> DCPResult<()> {
+    println!("{:#?}", config);
+    let docker = Docker::new("unix:///var/run/docker.sock")?;
+    let info = docker.info().await?;
+    println!("{:#?}", info);
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-fn main() {
-    if let Err(e) = dcp::get_args().and_then(dcp::run) {
-        eprintln!("{}", e);
-        std::process::exit(1)
+#[tokio::main]
+async fn main() -> dcp::DCPResult<()> {
+    match dcp::get_args() {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1)
+        }
+        Ok(config) => {
+            dcp::run(config).await?
+        }
     }
+
+    Ok(())
 }
